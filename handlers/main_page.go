4af9debe2f856365553ab04737dd76f9ea5ccf2e
efinauri <edoardo@finauri@gmail.com>
2024-06-27 11:31:49 +0200

    specify contentType header

--- handlers/main_page.go
+++ handlers/main_page.go
@@ -6,6 +6,7 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
